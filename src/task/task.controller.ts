aab943712372fdb1ebb1e87a039a45a63d0a9bb6
Juan Hernandez Puebla <al222010053@gmail.com>
2022-05-10 11:15:59 -0500

    Validacion

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task, TaskStatus } from './task.model';
 import { TaskService } from './task.service';
 
@@ -35,8 +36,9 @@ export class TaskController {
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id:string,
-    @Body('status') status:TaskStatus,
+    @Body() updateTaskStatusDto:UpdateTaskStatusDto,
   ): Task{
+    const {status} = updateTaskStatusDto;
     return this.tasksService.updateTaskStatus(id,status);
   }
 }
